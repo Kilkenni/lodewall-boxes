944e59b35c042978bc72b395fc15bf635afc4fc1
kilkenni <cgustu@gmail.com>
2016-06-21 19:45:51 +0300

    A working animation fully on local lua, thanks to Drotten!

--- components/chest/chest_component.lua
+++ components/chest/chest_component.lua
@@ -1,76 +1,21 @@
-local DoorComponent = class()
+local ChestComponent = class()
 
 local Point3 = _radiant.csg.Point3
 local Cube3 = _radiant.csg.Cube3
 
-local DOOR_FILTERS = {}
-
-local get_door_filter = function(door_entity)
-   local player_id = radiant.entities.get_player_id(door_entity)
-   local lockable = door_entity:get_component('stonehearth:door'):is_lockable()
-   local filter_id
-   if lockable then
-      filter_id = door_entity:get_id()
-   else
-      filter_id = player_id
-   end
-   local filter = DOOR_FILTERS[filter_id]
-   if not filter then
-      local filter_fn = function(entity)
-         local entity_player_id = radiant.entities.get_player_id(entity)
-         local is_friendly = stonehearth.player:are_player_ids_friendly(player_id, entity_player_id)
-         local door_component = door_entity and door_entity:get_component('stonehearth:door')
-         local is_locked = door_component and door_component:is_locked()
-         return is_friendly and not is_locked
-      end
-
-      local frc = stonehearth.ai:create_filter_result_cache(filter_fn, player_id .. ' door movement_guard_shape frc')
-      local amenity_changed_listener = radiant.events.listen(radiant, 'stonehearth:amenity:sync_changed', function(e)
-            local faction_a = e.faction_a
-            local faction_b = e.faction_b
-            if player_id == faction_a or player_id == faction_b then
-               if frc and frc.cache then
-                  frc.cache:clear()
-               end
-            end
-         end)
-      if lockable then
-         radiant.events.listen(door_entity, 'stonehearth:door:lock_changed', function(e)
-            if frc and frc.cache then
-               frc.cache:clear()
-            end
-         end)
-      end
-      filter = {
-         frc = frc,
-         listener = amenity_changed_listener
-      }
-      DOOR_FILTERS[filter_id] = filter
-   end
-   return filter
-end
-
-function DoorComponent:initialize()
-   self._sv.locked = nil;
+function ChestComponent:initialize()
    local json = radiant.entities.get_json(self)
    self._sensor_name = json.sensor
    self._tracked_entities = {}
 end
 
-function DoorComponent:activate(entity, json)
+function ChestComponent:activate()
    if self._sensor_name then
       self:_trace_sensor()
-      self:_add_collision_shape()
-      self._unit_info_trace = self._entity:add_component('unit_info')
-                                    :trace_player_id('door component')
-                                       :on_changed(function(player_id)
-                                             self:_on_player_id_changed()
-                                          end)
-                                       :push_object_state()
    end
 end
 
-function DoorComponent:destroy()
+function ChestComponent:destroy()
    if self._sensor_trace then
       self._sensor_trace:destroy()
       self._sensor_trace = nil
@@ -85,67 +30,13 @@ function DoorComponent:destroy()
       self._close_effect:stop()
       self._close_effect = nil
    end
-
-   if self._unit_info_trace then
-      self._unit_info_trace:destroy()
-      self._unit_info_trace = nil
-   end
 end
 
-function DoorComponent:toggle_lock()
-   local locked = self._sv.locked
-   if locked == nil then
-      locked = false
-   end
-   self._sv.locked = not locked
-   self.__saved_variables:mark_changed()
-   radiant.events.trigger(self._entity, 'stonehearth:door:lock_changed')
-end
-
-function DoorComponent:is_locked()
-   return self._sv.locked == true
-end
-
-function DoorComponent:is_lockable()
-   local commands = self._entity:get_component('stonehearth:commands')
-   return commands and commands:has_command('stonehearth:commands:toggle_lock')
-end
-
--- On load, the doors have a player id of "", so recalculate when
--- it can get the correct player id
-function DoorComponent:_on_player_id_changed()
-   local mgs = self._entity:get_component('movement_guard_shape')
-   local filter_data = get_door_filter(self._entity)
-   mgs:set_filter_result_cache(filter_data.frc.cache)
-end
-
-function DoorComponent:_add_collision_shape()
-   local portal = self._entity:get_component('stonehearth:portal')
-   if portal then
-      local mob = self._entity:add_component('mob')
-      local mgs = self._entity:add_component('movement_guard_shape')
-
-      local region2 = portal:get_portal_region()
-      local region3 = mgs:get_region()
-      if not region3 then
-         region3 = radiant.alloc_region3()
-         mgs:set_region(region3)
-      end
-      region3:modify(function(cursor)
-            cursor:clear()
-            for rect in region2:each_cube() do
-               cursor:add_unique_cube(Cube3(Point3(rect.min.x, rect.min.y,  0),
-                                            Point3(rect.max.x, rect.max.y,  1)))
-            end
-         end)
-   end
-end
-
-function DoorComponent:_trace_sensor()
+function ChestComponent:_trace_sensor()
    local sensor_list = self._entity:get_component('sensor_list')
    local sensor = sensor_list:get_sensor(self._sensor_name)
    if sensor then
-      self._sensor_trace = sensor:trace_contents('door')
+      self._sensor_trace = sensor:trace_contents('chest')
                                        :on_added(function (id, entity)
                                              self:_on_added_to_sensor(id, entity)
                                           end)
@@ -156,25 +47,25 @@ function DoorComponent:_trace_sensor()
    end
 end
 
-function DoorComponent:_on_added_to_sensor(id, entity)
+function ChestComponent:_on_added_to_sensor(id, entity)
    if self:_valid_entity(entity) then
       if not next(self._tracked_entities) and
          not self._sv.locked then
-         -- if this is in our faction, open the door
-         self:_open_door();
+         -- if this is in our faction, open the chest
+         self:_open_chest();
       end
       self._tracked_entities[id] = entity
    end
 end
 
-function DoorComponent:_on_removed_to_sensor(id)
+function ChestComponent:_on_removed_to_sensor(id)
    self._tracked_entities[id] = nil
    if not next(self._tracked_entities) then
-      self:_close_door()
+      self:_close_chest()
    end
 end
 
-function DoorComponent:_open_door()
+function ChestComponent:_open_chest()
    if self._close_effect then
       self._close_effect:stop()
       self._close_effect = nil
@@ -185,7 +76,7 @@ function DoorComponent:_open_door()
    end
 end
 
-function DoorComponent:_close_door()
+function ChestComponent:_close_chest()
    if self._open_effect then
       self._open_effect:stop()
       self._open_effect = nil
@@ -195,13 +86,13 @@ function DoorComponent:_close_door()
    end
 end
 
-function DoorComponent:_valid_entity(entity)
+function ChestComponent:_valid_entity(entity)
    if not entity then
       return false
    end
 
    if not radiant.entities.has_free_will(entity) then
-      -- entity can't open doors
+      -- entity can't open chests
       return false
    end
 
@@ -212,14 +103,8 @@ function DoorComponent:_valid_entity(entity)
    if radiant.entities.get_player_id(entity) ~= radiant.entities.get_player_id(self._entity) then
       return false
    end
-   
-   --[[
-   if not mob_component or not mob_component:get_moving() then
-      return false
-   end
-   ]]
 
    return true
 end
 
-return DoorComponent
+return ChestComponent
